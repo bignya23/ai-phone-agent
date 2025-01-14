6410cbed2065d6ecf035e5418e128509d1c1d374
bignya <146578548+bignya23@users.noreply.github.com>
2025-01-14 13:59:32 +0530

    Update app_web.py

--- app_web.py
+++ app_web.py
@@ -7,7 +7,7 @@ import src.text_to_speech
 import src.tools
 
 app = Flask(__name__)
-CORS(app, supports_credentials=True, origins=["https://ai-phone-agent-1.onrender.com"])
+CORS(app)
 
 conversation_history = ""
 user_input = ""
